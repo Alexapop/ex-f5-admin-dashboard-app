77d0367d96949188a203ee9cc99cabdb6e177cbd
Ioana <ioana3popescu@gmail.com>
2026-06-08 20:25:35 +0200

    feat:implement admin login and session flow

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
+import { initLoginPage } from "./src/js/auth.js";
+import { isLoggedIn } from "./src/js/session.js";
+
 const page = document.body.dataset.page;
 
 if (page === "index") {
-  const isLoggedIn = localStorage.getItem("isLoggedIn");
-
-  if (isLoggedIn === "true") {
+  if (isLoggedIn()) {
     window.location.href = "./src/pages/active-session.html";
   } else {
     window.location.href = "./src/pages/login-page.html";
   }
 }
 
+if (page === "login") {
+  initLoginPage();
+}
+
 if (page === "active-session") {
   setTimeout(() => {
     window.location.href = "./employee-directory.html";
   }, 2500);
+}
+
+if (page === "employees") {
+  if (!isLoggedIn()) {
+    window.location.href = "./login-page.html";
+  }
 }
