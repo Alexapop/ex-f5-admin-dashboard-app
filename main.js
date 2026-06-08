f59b6ecec66e70cac80beaf3068c3304e307ae91
Ioana <ioana3popescu@gmail.com>
2026-06-08 18:55:52 +0200

    connect pages to main script

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+const page = document.body.dataset.page;
 
+if (page === "index") {
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
 
-const isLoggedIn = localStorage.getItem("isLoggedIn");
+  if (isLoggedIn === "true") {
+    window.location.href = "./src/pages/active-session.html";
+  } else {
+    window.location.href = "./src/pages/login-page.html";
+  }
+}
 
-if (isLoggedIn === "true") {
-  window.location.href = "./src/pages/active-session.html";
-} else {
-  window.location.href = "./src/pages/login-page.html";
+if (page === "active-session") {
+  setTimeout(() => {
+    window.location.href = "./employee-directory.html";
+  }, 2500);
 }
